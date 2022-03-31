1bb4984e3a21e77466ed802ae68acf3013ed4ba8
Kain8tp8 <98201571+Kain8tp8@users.noreply.github.com>
2022-03-31 16:10:50 +0500

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,22 @@ app.set("view engine", ".hbs");
 app.set("views", "./pages");
 
 app.get("/", (req, res) => {
-  res.render("index", {title:'Home'});
+  res.render("index", { title: "Home", juratbek: "*" });
 });
 app.get("/about", (req, res) => {
-  res.render("about", {title:'About'});
+  res.render("about", { title: "About", muxtor: "*" });
 });
+
+let phone = [
+  { name: "Javahir",  number: '97 - 364 - 51 - 59' },
+  { name: "Olimjon",  number: '88 - 517 - 72 - 66' },
+  { name: "Juratbek", number: '93 - 467 - 12 - 70' },
+  { name: "Muhtor",   number: '93 - 123 - 45 - 67' },
+  { name: "Abror",    number: '91 - 916 - 46 - 45' },
+  { name: "Temur",    number: '94 - 239 - 43 - 88' },
+];
 app.get("/contact", (req, res) => {
-  res.render("contact", {title:'Comtact'});
+  res.render("contact", {contacts: phone, title: "Comtact", timur: "*" });
 });
 
 app.listen(888, () => {
